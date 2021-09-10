b48603f4d6a1c7d37de2f043c551087d9f51ca45
Eliezer <reuvene@mprest.com>
2021-09-10 08:34:23 +0300

    ack

--- src/main/java/com/kafka/config/KafkaTopicConfig.java
+++ src/main/java/com/kafka/config/KafkaTopicConfig.java
@@ -1,40 +1,40 @@
-package com.kafka.config;
-
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.KafkaAdmin;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@Configuration
-public class KafkaTopicConfig {
-
-    @Value(value = "${kafka.bootstrap.address}")
-    private String bootstrapAddress;
-
-    @Value(value = "${kafka.bootstrap.port}")
-    private String bootstrapPort;
-
-    @Value(value = "${kafka.topicName}")
-    private String topicName;
-
-    @Bean
-    public KafkaAdmin kafkaAdmin() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress + ":" + bootstrapPort);
-        return new KafkaAdmin(configs);
-    }
-
-    @Bean
-    public NewTopic topic1() {
-        return new NewTopic(topicName, 1, (short) 1);
-    }
-
-
-}
+//package com.kafka.config;
+//
+//import org.apache.kafka.clients.admin.AdminClientConfig;
+//import org.apache.kafka.clients.admin.NewTopic;
+//import org.apache.kafka.clients.consumer.ConsumerConfig;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+//import org.springframework.kafka.core.KafkaAdmin;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@Configuration
+//public class KafkaTopicConfig {
+//
+//    @Value(value = "${kafka.bootstrap.address}")
+//    private String bootstrapAddress;
+//
+//    @Value(value = "${kafka.bootstrap.port}")
+//    private String bootstrapPort;
+//
+//    @Value(value = "${kafka.topicName}")
+//    private String topicName;
+//
+//    @Bean
+//    public KafkaAdmin kafkaAdmin() {
+//        Map<String, Object> configs = new HashMap<>();
+//        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress + ":" + bootstrapPort);
+//        return new KafkaAdmin(configs);
+//    }
+//
+//    @Bean
+//    public NewTopic topic1() {
+//        return new NewTopic(topicName, 1, (short) 1);
+//    }
+//
+//
+//}
